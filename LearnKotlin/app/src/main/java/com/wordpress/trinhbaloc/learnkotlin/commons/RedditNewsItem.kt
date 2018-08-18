302ab69ad7c1f7766f29512ea28658d0f676a98e
Trinh Loc <t>
2018-08-18 22:48:05 +0700

    RedditNewsItem

---
+++ LearnKotlin/app/src/main/java/com/wordpress/trinhbaloc/learnkotlin/commons/RedditNewsItem.kt
@@ -0,0 +1,16 @@
+package com.wordpress.trinhbaloc.learnkotlin.commons
+import com.wordpress.trinhbaloc.learnkotlin.commons.adapter.AdapterConstants
+import com.wordpress.trinhbaloc.learnkotlin.commons.adapter.ViewType
+
+
+data class RedditNewsItem(
+        val author: String,
+        val title: String,
+        val numComments: Int,
+        val created: Long,
+        val thumbnail: String,
+        val url: String
+): ViewType{
+    override fun getViewType() = AdapterConstants.NEWS
+
+}
