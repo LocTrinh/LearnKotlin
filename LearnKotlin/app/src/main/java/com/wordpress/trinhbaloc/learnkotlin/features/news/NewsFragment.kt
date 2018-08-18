2fa477bc9a97feee321047e38840b5238e5d4102
Trinh Loc <t>
2018-08-18 22:00:23 +0700

    add

--- LearnKotlin/app/src/main/java/com/wordpress/trinhbaloc/learnkotlin/features/news/NewsFragment.kt
+++ LearnKotlin/app/src/main/java/com/wordpress/trinhbaloc/learnkotlin/features/news/NewsFragment.kt
@@ -10,18 +10,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.wordpress.trinhbaloc.learnkotlin.R
 import com.wordpress.trinhbaloc.learnkotlin.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
-    private var newsList: RecyclerView? = null
+    private val newsList by lazy {
+        news_list
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //val view = inflater.inflate(R.layout.news_fragment, container, false)
-        val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true) // use this setting to improve performance
-        newsList?.layoutManager = LinearLayoutManager(context)
+        return container?.inflate(R.layout.news_fragment)
+    }
 
-        return view
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
+
     }
 }
