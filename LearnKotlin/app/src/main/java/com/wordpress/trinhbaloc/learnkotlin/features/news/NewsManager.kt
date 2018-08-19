58eb8191f724af574ae2d8021393d12c21f2e05b
Trinh Loc <t>
2018-08-19 11:10:22 +0700

    add api reddit

--- LearnKotlin/app/src/main/java/com/wordpress/trinhbaloc/learnkotlin/features/news/NewsManager.kt
+++ LearnKotlin/app/src/main/java/com/wordpress/trinhbaloc/learnkotlin/features/news/NewsManager.kt
@@ -1,5 +1,6 @@
 package com.wordpress.trinhbaloc.learnkotlin.features.news
 
+import com.wordpress.trinhbaloc.learnkotlin.api.RestAPI
 import com.wordpress.trinhbaloc.learnkotlin.commons.RedditNewsItem
 import rx.Observable
 
@@ -8,25 +9,28 @@ import rx.Observable
  *
  * @author juancho
  */
-class NewsManager() {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+
+    fun getNews(limit: String = "100"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
-            subscriber.onCompleted()
+
         }
     }
 }
